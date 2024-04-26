e96296a6e3c91f44e8ff63c8b21633636829a178
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-04-26 20:50:11 +0530

    Update keycloak.server.ts

--- frameworks/remix/app/services/keycloak.server.ts
+++ frameworks/remix/app/services/keycloak.server.ts
@@ -1,13 +1,15 @@
 import { KeycloakStrategy } from "remix-keycloak";
 
-export default new KeycloakStrategy(
-  {
+const kcConfig = {
     useSSL: true,
     domain: "usw2.auth.ac/auth",
     realm: "shared-deployment-001",
     clientID: "reg-example-1",
     clientSecret: "CLIENT_SECRET",
     callbackURL: "http://localhost:3000/auth/keycloak/callback",
-  },
-  async ({ profile }) => profile
+}
+
+export default new KeycloakStrategy(
+  kcConfig,
+  ({ profile }) => profile
 );
