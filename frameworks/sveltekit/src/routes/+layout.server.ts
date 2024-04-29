f3829a4315a296214a19a2111e6d9367df925398
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-04-29 16:35:31 +0530

    more changes

--- frameworks/sveltekit/src/routes/+layout.server.ts
+++ frameworks/sveltekit/src/routes/+layout.server.ts
@@ -2,7 +2,6 @@ import type { LayoutServerLoad } from "./$types.js";
 
 export const load: LayoutServerLoad = async (event) => {
   const session = await event.locals.getSession();
-  console.log({ session });
   return {
     session,
   };
