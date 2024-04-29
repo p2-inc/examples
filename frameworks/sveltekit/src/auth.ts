1c680dfd1e85e2ca147356d765c4a6df7cb526d6
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-04-29 16:46:31 +0530

    Update auth.ts

--- frameworks/sveltekit/src/auth.ts
+++ frameworks/sveltekit/src/auth.ts
@@ -1,18 +1,22 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import Keycloak from "@auth/sveltekit/providers/keycloak";
-import {
-  AUTH_KEYCLOAK_ID,
-  AUTH_KEYCLOAK_ISSUER,
-  AUTH_KEYCLOAK_SECRET,
-} from "$env/static/private";
+// import {
+//   AUTH_KEYCLOAK_ID,
+//   AUTH_KEYCLOAK_ISSUER,
+//   AUTH_KEYCLOAK_SECRET,
+// } from "$env/static/private";
+
+const realm = "shared-deployment-001";
+
+const kcConfig = {
+      issuer: `https://usw2.auth.ac/auth/realms/${realm}`,
+      clientId: "reg-example-1",
+      clientSecret: "CLIENT_SECRET", // Paste "Client secret" here. Use Environment variables in prod
+}
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
   trustHost: true,
   providers: [
-    Keycloak({
-      clientId: AUTH_KEYCLOAK_ID,
-      issuer: AUTH_KEYCLOAK_ISSUER,
-      clientSecret: AUTH_KEYCLOAK_SECRET,
-    }),
+    Keycloak(kcConfig),
   ],
 });
