47155644407392f913715ca78281b89931c3c35a
pnzrr <pnzrrr@pm.me>
2024-07-18 06:27:19 -0600

    update to use env secrets

--- frameworks/nextjs/src/lib/auth.ts
+++ frameworks/nextjs/src/lib/auth.ts
@@ -1,17 +1,23 @@
 import { NextAuthOptions } from "next-auth";
 import KeycloakProvider from "next-auth/providers/keycloak";
 
-const authServerUrl = "https://euc1.auth.ac/auth/";
-const realm = "shared-deployment-001";
-const clientId = "reg-example-1";
-const clientSecret = "CLIENT_SECRET";
+const authServerUrl = process.env.KEYCLOAK_ISSUER;
+const clientId = process.env.KEYCLOAK_ID!;
+const clientSecret = process.env.KEYCLOAK_SECRET!;
+
+// Example .env values
+// NEXTAUTH_URL=http://localhost:3000 # URL of the Next.js app
+// NEXTAUTH_SECRET=<secret> # Client secret from Keycloak client
+// KEYCLOAK_ID=nextjs # Keycloak client ID
+// KEYCLOAK_SECRET=<secret> # Client secret from Keycloak client
+// KEYCLOAK_ISSUER=https://app.phasetwo.io/auth/realms/p2examples # Url of the Keycloak server
 
 export const AuthOptions: NextAuthOptions = {
   providers: [
     KeycloakProvider({
       clientId,
       clientSecret,
-      issuer: `${authServerUrl}realms/${realm}`,
+      issuer: authServerUrl,
     }),
   ],
 };
